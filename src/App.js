5bbb5d4863e57c4f4de933e6caee5cea0b236847
Peter Sekesan <psekesan@gmail.com>
2021-06-23 14:14:40 -0400

    nav routes and switches added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import NavMenu from './components/NavMenu';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+
+
 
 export default function App() {
   return (
     <>
-      <NavMenu/>
-      <h1>Peter Sekesan</h1>
-      
+      <Router>
+        <NavMenu />
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
+        
+        <Home  />
+        </Router>
     </>
   );
 }
--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
 
 function NavMenu() {
     return (
+     
         <div>
-          <h1>This is the NavMenu</h1>  
+            <ul>
+                <li><NavLink to ="/">Home</NavLink></li>
+                <li><NavLink to ="/about">About</NavLink></li>
+                <li><NavLink to ="/projects">Projects</NavLink></li>
+                <li><NavLink to ="/contact">Contact</NavLink></li>
+            </ul>
         </div>
+        
     )
 }
 
